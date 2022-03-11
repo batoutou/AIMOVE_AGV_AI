fbf7f4cf97718f037b979e0391b96e839f202388
Francois-37 <67963661+Francois-37@users.noreply.github.com>
2022-03-11 20:33:07 +0100

    add_output_gautier

--- IA_AGV/main.py
+++ IA_AGV/main.py
@@ -18,18 +18,13 @@ from manager.actions import *
 
 # Mode = "TRAIN_MODE"
 Mode = "TEST_MODE"
-Model = "FCNN"  # GMM
 FPS_selected = 10
 ratio_image = 1.3
 path_train = "./data/train"
-path_test = './data/test'
-path_train_pkl= './data/save_train_data.pkl'
-
-time_frame = collections.deque(maxlen=30)
-
+path_test = "./data/test"
+path_train_pkl= "./data/save_train_data.pkl"
 previousTime = 0
 
-buff_np=np.zeros([30,42])
 
 if (Mode == "TRAIN_MODE"):
     if os.path.isfile(path_train_pkl) == True:  #Si y a des données dans le pickle
@@ -38,29 +33,21 @@ if (Mode == "TRAIN_MODE"):
         print("No data in the pinkle file")
         X_train, Y_train = data_extraction(path_train)
         save_pickle([X_train, Y_train], path_train_pkl)
-
-    # if(Model == "GMM"):
-    #     train_arrays = arrange_data_for_GMM(train_arrays)
-    #     GMM = train_model_GMM(train_arrays)
-    #     print("GMM trained")
     
-    if(Model == "FCNN"):
-        FCNN = train_model_FCNN(X_train, Y_train)
-        print("FCNN trained")
-        FCNN.save('FCNN/FCNN_model')
-        print("FCNN saved")
+    FCNN = train_model_FCNN(X_train, Y_train)
+    print("FCNN trained")
+    FCNN.save('FCNN/FCNN_model')
+    print("FCNN saved")
         
-if(Mode == "TEST_MODE"):
-    FCNN = keras.models.load_model('FCNN/FCNN_model')
+if(Mode == "TEST_MODE"): FCNN = keras.models.load_model(r'IA_AGV\FCNN\FCNN_model')
 
-classes = class_extract('data/train')
+classes = class_extract(r'IA_AGV\data\train')
 print("classes : ",classes)
 # sys.exit()
 
-cap = cv2.VideoCapture(1)   # For webcam input, 0 original webcam, q1 extern webcam
+cap = cv2.VideoCapture(0)   # For webcam input, 0 original webcam, q1 extern webcam
 
-# To order the actions
-act = action(classes)
+act = action(classes)  # To order the actions
 
 while True:#cap.isOpened():
 
@@ -73,20 +60,15 @@ while True:#cap.isOpened():
     hands = mp_model()
     
     image.flags.writeable = True
-    
-    # results = hands.process(image)q
+
     image, results = mediapipe_detection(image, hands)
 
     image = draw_landmark(image, results)
 
     list_joints_image = exctract_joint_image(image, results)
-    #feature_vectorq
 
-    # list_joints_image = normlization(list_joints_image)
-    
+    C="No class detected"
 
-    #print(np.array(list_joints_image).shape)
-    C="Pas de classe bro"
     if(len(list_joints_image)==42):
         list_joints_image=pre_process_landmark(list_joints_image)
 
@@ -95,30 +77,11 @@ while True:#cap.isOpened():
         C=classes[int(probs)]
     C = mean_classes(C,classes)
 
-    message = act.add_action(C)
+    message_cv2, message_robot = act.add_action(C)
 
-    #predict_model_GMM(GMM, np.array(list_joints_image))
-    
-    # idx+=1
-    
-    # if (idx>30):
-    #     resultats=[]
-    #     for hmm_model_num in range(len(hmm_models)):
-    #         res=hmm_models[hmm_model_num].score(np.array(buff_np))
-    #         resultats.append(res)
-    # print(resultats)
-    # ide=resultats.index(max(resultats))
-    # print("Best model : ", classes[ide])
+    if(message_robot!=-1): print(message_robot)
 
-
-    # seuil=-20000
-    # if (max(resultats) > seuil):
-    #   ide=resultats.index(max(resultats))
-    #   print("Best model : ", classes[ide])
-    # else:
-    #   print("No matching signs ")
-
-    time_prog, previousTime, image = FPS(message, C, previousTime, image)
+    time_prog, previousTime, image = FPS(message_cv2, C, previousTime, image)
 
     image = cv2.resize(image, (0,0), fx=ratio_image, fy=ratio_image) 
 
--- IA_AGV/manager/actions.py
+++ IA_AGV/manager/actions.py
@@ -1,31 +1,23 @@
-import os
-
-class pos:
-    def __init__(self,x,y):
-        self.x=x
-        self.y=y
-
 class action:
     def __init__(self, classes):
         self.action="No action"
         self.next_poste="No poste"
+        self.next_poste_int=0
         self.classes=classes
         self.stop=0
         self.message=""
-        # self.position=[]
-
-    # def add_pos():
-    #     self.position.append()
 
     def add_action(self, current_classe):
-        # print("current_classe : ",current_classe)
+        last_state=self.next_poste_int
         if current_classe==self.classes[0]: #stop
             self.message="Robot stopped !"
+            self.next_poste_int="STOP"
             self.stop=1
 
         if self.action=="Validation_step": #validation step
             if current_classe == self.classes[-1]:
                 self.message=str("Let's go to the station " + self.next_poste+"!")
+                self.next_poste_int=self.next_poste
                 self.action="Action"
             if current_classe == self.classes[-2]:
                 self.message="Action canceled !"
@@ -35,29 +27,9 @@ class action:
             self.action="Validation_step"
             self.next_poste=current_classe
             self.message=str("Do you validate station "+current_classe+ "?")
-        return self.message
 
+        if(last_state!=self.next_poste_int): 
+            return self.message, self.next_poste_int
+        return self.message, -1
 
-
-
-
-#test
-if __name__ == '__main__':
-    
-    def class_extract(path):
-        classes = next(os.walk( path) )
-        classes=classes[1]
-        return classes
-
-    print(class_extract('data/train'))
-    A=action(class_extract('data/train')) #["Stop","1","2","oui","non"])
-
-    print(A.classes)
-
-    A.add_action(A.classes[2])
-    A.add_action(A.classes[3])
-    A.add_action(A.classes[-2])
-    A.add_action(A.classes[4])
-    A.add_action(A.classes[-1])
-    A.add_action(A.classes[0])
-
+# if __name__ == '__main__':
